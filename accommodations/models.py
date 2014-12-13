6e41778d19ffe30920d719d528f2323bc8eae6c0
alexugoku <alexugoku@gmail.com>
2014-12-13 12:40:09 +0200

    -  modified layout -  modified views, forms - deleted cerere.html (is now in index.html)

--- accommodations/models.py
+++ accommodations/models.py
@@ -6,15 +6,18 @@ class Dorm(models.Model):
     room_numbers = models.IntegerField(default=0)
     faculty = models.CharField(max_length=40)
 
+
 class Room(models.Model):
     number = models.IntegerField(default=0)
     size = models.IntegerField(default=0)
 
     accomodation = models.ForeignKey(Dorm)
 
+
 class Application(models.Model):
     publication_date = models.DateTimeField()
     dorms = models.OneToOneField(Dorm)
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
@@ -24,16 +27,15 @@ class Student(models.Model):
     address = models.CharField(max_length=60)
     email = models.EmailField()
     grade = models.IntegerField(default=0)
-    social_case = models.BooleanField(default=False) # social cases are treated separately
+    social_case = models.BooleanField(default=False)  # social cases are treated separately
     year = models.IntegerField(default=1)
 
-#    previous_room = models.ForeignKey(Room) # crashes with current_room
+    # previous_room = models.ForeignKey(Room) # crashes with current_room
     current_room = models.ForeignKey(Room)
 
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
-
 
 
 class Documents(models.Model):
