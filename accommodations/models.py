8def234d12324b3507dde0278856de7b9ae818cd
Iustin <iustin_bulimar@yahoo.com>
2014-12-13 16:02:47 +0200

    Update models to use a custom user object. Please rm and run syncdb again.

--- accommodations/models.py
+++ accommodations/models.py
@@ -1,10 +1,12 @@
+from django.utils import timezone
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 class Dorm(models.Model):
     name = models.CharField(max_length=40)
     room_numbers = models.IntegerField(default=0) # classic rooms
-    social_room_numbers = model.IntegerField(default=0) # rooms for social cases
+    social_room_numbers = models.IntegerField(default=0) # rooms for social cases
     faculty = models.CharField('Faculty', max_length=40)
     description = models.TextField()
     picture = models.FileField(upload_to='uploads')
@@ -30,7 +32,37 @@ class Application(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='New')
     comment = models.TextField()
 
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, last_login=now,
+                          is_active=True, is_staff=is_staff,
+                          is_superuser=is_superuser,
+                          **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
+
+
+
 class Student(AbstractBaseUser):
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
@@ -46,6 +78,7 @@ class Student(AbstractBaseUser):
     current_room = models.ForeignKey('Room', related_name='this_year_students', null=True)
 
     USERNAME_FIELD = 'email'
+    objects = UserManager()
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
