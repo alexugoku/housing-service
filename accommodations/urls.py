fc83b87ae4706b719de35893417b86f31b7f381d
alexugoku <alexugoku@gmail.com>
2014-12-13 11:41:33 +0200

    - test: added layout

--- accommodations/urls.py
+++ accommodations/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'accommodations.views.home', name='home'),
+    url(r'^$', 'accommodations.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
