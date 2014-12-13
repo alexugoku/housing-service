0286fe75ec0e5aded9faca2b35dcd045038d6f78
alexugoku <alexugoku@gmail.com>
2014-12-13 11:59:12 +0200

    -  modified layout - added login logout views

--- accommodations/views.py
+++ accommodations/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
-
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+def camine(request):
+    return render(request, '_layout_camin.html', {})
+
+
+def cerere(request):
+    return render(request, 'cerere.html', {})
+
+def login_view(request):
+    if request.method == 'GET':
+        login_form = UserLogin()
+        context = {
+            'form': login_form,
+        }
+        return render(request, 'login.html', context)
+    if request.method == 'POST':
+        login_form = UserLogin(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            context = {
+                'form': login_form,
+                'message': 'Wrong user and/or password!',
+            }
+            return render(request, 'login.html', context)
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('login'))
